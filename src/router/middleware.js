350034a76b2bc7823d1ca50ae7ccb3740dc216a0
vlad <vlad.legusha@gmail.com>
2020-08-25 02:57:34 +0300

    fix router middleware.js

--- src/router/middleware.js
+++ src/router/middleware.js
@@ -1,6 +1,9 @@
 export default {
   postEdit (store, to, from, next) {
+    const defaultPostPage = '1'
+    const defaultPage = { name: 'Posts', params: { page: defaultPostPage } }
     const isHasId = to.params?.id
+
     if (isHasId) {
       const radix = 10
       const id = parseInt(to.params.id, radix) || false // if not parse to integer,expected false instead NaN
@@ -11,10 +14,10 @@ export default {
         next()
         return
       }
-      next({ name: 'Posts' })
+      next(defaultPage)
       return
     }
-    next({ name: 'Posts' })
+    next(defaultPage)
   },
   postCreate (store, to, from, next) {
     store.commit('setCurrentPost')
